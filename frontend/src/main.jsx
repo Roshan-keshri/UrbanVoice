053777a3e43e370603b1ab9a57679a91c1e682df
Roshan-keshri <keshriroshan44@gmail.com>
2026-07-07 03:51:34 +0530

    some bugs fixing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,10 @@ import "./index.css";
 
 // ✅ Import AuthContext
 import { AuthProvider } from "./context/AuthContext";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
